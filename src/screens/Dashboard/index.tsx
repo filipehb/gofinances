6c80f9a1ab346d2526dfeb22793266bf3443ade0
Filipe Arruda <filipehenriquester@gmail.com>
2022-02-24 00:04:07 -0300

    - Adicionado o responsive font size - Criado os styleds components para o header, userwrapper, userinfo, user, photo, user greeting, username e icon

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,32 @@
 import React from "react";
-import { Container, Title } from "./styles";
+
+import {
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    User,
+    Photo,
+    UserGreeting,
+    UserName,
+    Icon
+} from "./styles";
 
 export function Dashboard() {
     return (
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/869359?v=4' }} />
+                        <User>
+                            <UserGreeting>Olá, </UserGreeting>
+                            <UserName>Filipe</UserName>
+                        </User>
+                    </UserInfo>
+                    <Icon name="power"/>
+                </UserWrapper>
+            </Header>
         </Container>
     )
 }
