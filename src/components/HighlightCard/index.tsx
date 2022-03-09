6ceadcb53cbcafd139e7e07336c0933807005a49
Filipe Arruda <filipehenriquester@gmail.com>
2022-03-08 23:39:46 -0300

    - Adicionada interface para as propriedades dos cards -Importado para os estilos as propriedades -Adicionado tratamento para cada tipo de card

--- src/components/HighlightCard/index.tsx
+++ src/components/HighlightCard/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { 
+import {
     Container,
     Header,
     Title,
@@ -10,18 +10,31 @@ import {
     LastTransaction
 } from "./styles";
 
-export function HighlightCard() {
-    return(
-        <Container>
+interface Props {
+    title: string;
+    amount: string;
+    lastTransaction: string;
+    type: 'up' | 'down' | 'total';
+}
+
+const icon = {
+    up: 'arrow-up-circle',
+    down: 'arrow-down-circle',
+    total: 'dollar-sign',
+}
+
+export function HighlightCard({ type, title, amount, lastTransaction }: Props) {
+    return (
+        <Container type={type}>
             <Header>
-                <Title>Entrada</Title>
-                <Icon name="arrow-up-circle"/>
+                <Title type={type}>{title}</Title>
+                <Icon name={icon[type]} type={type} />
             </Header>
 
             <Footer>
-                <Amount>R$ 17.400,00</Amount>
-                <LastTransaction>
-                    Última entrada dia 13 de abril
+                <Amount type={type}>{amount}</Amount>
+                <LastTransaction type={type}>
+                    {lastTransaction}
                 </LastTransaction>
             </Footer>
         </Container>
